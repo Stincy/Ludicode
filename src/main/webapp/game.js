be8d142bb25ff5ce307aa32df66bf70887140578
Damien Masson <damien.masson.a@gmail.com>
2015-01-13 11:49:45 +0100

    Dessin du perso

--- src/main/webapp/game.js
+++ src/main/webapp/game.js
@@ -2,10 +2,9 @@ function Map(tiles, width, height) {
 	this.tiles = tiles;
 	this.width = width;
 	this.height = height;
+	this.tile_size = (width/tiles.length);
 	
 	this.render = function render(context) {
-		var tile_size = (width/tiles.length);
-
 		for (var xx = 0; xx < tiles.length; ++xx) {
 			for (var yy = 0; yy < tiles[0].length; ++yy) {
 				if (tiles[yy][xx] == 0) {
@@ -13,22 +12,24 @@ function Map(tiles, width, height) {
 				} else {
 					context.fillStyle = "#00FF00";
 				}
-				context.fillRect(xx*tile_size, yy*tile_size, tile_size, tile_size);
-				context.strokeRect(xx*tile_size, yy*tile_size, tile_size, tile_size);
+				context.fillRect(xx*this.tile_size, yy*this.tile_size, this.tile_size, this.tile_size);
+				context.strokeRect(xx*this.tile_size, yy*this.tile_size, this.tile_size, this.tile_size);
 			}
 		}
 	}
 }
 
 
-function Player(x, y) {
+function Player(map, x, y) {
 	this.x = x;
 	this.y = y;
+	this.map = map;
 
 	this.render = function render(context) {
 		context.beginPath();
-		context.arc(x, y, 40,0,2*Math.PI);
-		context.stroke();
+		context.arc(x+map.tile_size/2, y+map.tile_size/2, map.tile_size/2 - 4,0 , 2*Math.PI);
+		context.fillStyle = "#FF0000";
+		context.fill();
 	}
 }
 
@@ -42,7 +43,7 @@ function Game(width, height) {
                       [0,0,0,0]];
 
 	this.map = new Map(this.tiles, this.width, this.height);
-	this.player = new Player(0, 0);
+	this.player = new Player(this.map, 0, 0);
 
 	this.render = function render() {
 		var c = document.getElementById("enigme");
