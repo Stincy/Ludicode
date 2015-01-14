b87d16fad2d4368e5c4cd95350988f8268e4a01e
Damien Masson <damien.masson.a@gmail.com>
2015-01-14 12:00:45 +0100

    Ajout de la classe de DB

---
+++ src/main/java/BDD/Database.java
@@ -0,0 +1,111 @@
+package BDD;
+
+import java.io.File;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+
+import org.sqlite.SQLiteConfig;
+
+public class Database {
+
+	private static Connection db;
+	private static final String file = "database.db";
+
+
+	public static Connection getConnection() throws Exception {
+		try {
+			if (db == null || db.isClosed()) {
+				File dir = new File(file);
+				boolean freshlyCreated = !dir.exists();
+				
+				Class.forName("org.sqlite.JDBC");
+				SQLiteConfig config = new SQLiteConfig();
+				config.enforceForeignKeys(true);
+				db = DriverManager.getConnection("jdbc:sqlite:data/blender.db", config.toProperties());
+				if (freshlyCreated) initDb();
+			}
+		} catch (ClassNotFoundException e) {
+			throw new Exception("Le driver de base de donnees est introuvable");
+		} catch (SQLException e) {
+			throw new Exception("La connexion a la base de donnees a echoue");
+		}
+
+		return db;
+	}
+
+	public static void closeConnection() {
+		try {
+			db.close();
+		} catch (SQLException e) {}
+	}
+
+
+	public static void initDb() throws Exception {
+		Connection c = getConnection();
+	    Statement stmt = null;
+	    try {
+
+	      stmt = c.createStatement();
+	      String sql = "CREATE TABLE Historique " +
+	                   "(NumHisto    INT NOT NULL," +
+	                   " Score       INT NOT NULL," +
+	                   " TypeHisto   TEXT NOT NULL," +
+	                   " Pseudo      TEXT NOT NULL," +
+	                   " NumProbleme INT NOT NULL," +
+	                   " CONSTRAINT NumProbleme_fk FOREIGN KEY(NumProbleme) REFERENCES Probleme," +
+	                   " CONSTRAINT Pseudo_fk FOREIGN KEY(Pseudo) REFERENCES Utilisateur" +
+	                   " PRIMARY KEY (NumHisto, Score))";
+	      stmt.executeUpdate(sql);
+	      
+	      stmt = c.createStatement();
+	      sql = "CREATE TABLE Leaderboard " +
+	                   "(Pseudo TEXT," +
+	                   " Score  INT," + 
+	                   " CONSTRAINT Pseudo_fk FOREIGN KEY(Pseudo) REFERENCES Utilisateur," +
+	                   " CONSTRAINT Score_fk  FOREIGN KEY(Score) REFERENCES Historique," +
+	                   " PRIMARY KEY (Pseudo, Score))"; 
+	      stmt.executeUpdate(sql);
+	      
+	      stmt = c.createStatement();
+	      sql = "CREATE TABLE Probleme " +
+	                   "(NumProbleme INT PRIMARY KEY  NOT NULL," +
+	                   " TypePb                  TEXT NOT NULL," + 
+	                   " NiveauDiff              INT  NOT NULL," +
+	                   " TaillePb                TEXT NOT NULL," +
+	                   " Enonce                  TEXT NOT NULL," +
+	                   " Score                   INT  NOT NULL," +
+	                   " NumHisto                INT  NOT NULL," +
+	                   " Pseudo                  TEXT NOT NULL," +
+	                   " CONSTRAINT Pseudo_fk FOREIGN KEY(Pseudo) REFERENCES Utilisateur," +
+	                   " CONSTRAINT NumHisto_fk FOREIGN KEY(NumHisto) REFERENCES Historique," +
+	                   " CONSTRAINT Score_fk FOREIGN KEY(Score) REFERENCES Utilisateur)";
+	      stmt.executeUpdate(sql);
+	      
+	      stmt = c.createStatement();
+	      sql = "CREATE TABLE Utilisateur " +
+	                   "(Pseudo1 TEXT PRIMARY KEY     NOT NULL," +
+	                   " Nom                  TEXT    NOT NULL," + 
+	                   " Prenom               INT     NOT NULL," +
+	                   " Mdp                  TEXT    NOT NULL," +
+	                   " TypeUser             TEXT    NOT NULL," +
+	                   " Pseudo2              TEXT    NOT NULL," +
+	                   " CONSTRAINT Pseudo2_fk FOREIGN KEY(Pseudo2) REFERENCES Probleme)";
+	      stmt.executeUpdate(sql);
+	      
+	      stmt.close();
+	      c.close();
+	    } catch ( Exception e ) {
+	      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+	    }
+	}
+}
