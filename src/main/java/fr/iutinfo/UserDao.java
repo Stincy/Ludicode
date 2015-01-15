00b59e20ecd6d5dd9389b4c2e8a22b40d12a6b8b
YOANN LAMEIRE <yoann.lameire@etudiant.univ-lille1.fr>
2015-01-15 14:53:32 +0100

    Commit pour pull

--- src/main/java/fr/iutinfo/UserDao.java
+++ src/main/java/fr/iutinfo/UserDao.java
@@ -10,33 +10,32 @@ import org.skife.jdbi.v2.tweak.BeanMapperFactory;
 public interface UserDao {
 
 	@SqlUpdate("CREATE TABLE Historique " +
-	                   "(NumHisto    INT PRIMARY KEY NOT NULL," +
+	                   "(NumHisto    INT PRIMARY KEY AUTOINCREMENT," +
 	                   " Score       INT NOT NULL," +
-	                   " TypeHisto   TEXT NOT NULL," +
 	                   " Pseudo      TEXT NOT NULL," +
-	                   " NumProbleme INT NOT NULL," +
-	                   " CONSTRAINT NumProbleme_fk FOREIGN KEY(NumProbleme) REFERENCES Probleme," +
-	                   " CONSTRAINT Pseudo_fk FOREIGN KEY(Pseudo) REFERENCES Utilisateur);")
+	                   " Numlevel    INT NOT NULL," +
+	                   " CONSTRAINT Numlevel_fk FOREIGN KEY(id) REFERENCES levels," +
+	                   " CONSTRAINT NumUt_fk FOREIGN KEY(NumUt) REFERENCES Utilisateur);")
 	void createHistoriqueTable();
 	
 	@SqlUpdate("CREATE TABLE Leaderboard " +
-            "(Pseudo TEXT," +
+            "(NumUt  INT," +
             " Score  INT," + 
-            " CONSTRAINT Pseudo_fk FOREIGN KEY(Pseudo) REFERENCES Utilisateur," +
+            " CONSTRAINT NumUt_fk FOREIGN KEY(NumUt) REFERENCES Utilisateur," +
             " CONSTRAINT Score_fk  FOREIGN KEY(Score) REFERENCES Historique," +
             " PRIMARY KEY (Pseudo, Score));")
 	void createLeaderboardTable();
 	
 	
 	@SqlUpdate("CREATE TABLE Utilisateur " +
-	                   "(NumUt    INT PRIMARY KEY" +
+	                   "(NumUt    INT PRIMARY KEY AUTOINCREMENT" +
 	                   " Pseudo1              TEXT    NOT NULL," +
 	                   " Nom                  TEXT    NOT NULL," + 
 	                   " Prenom               INT     NOT NULL," +
 	                   " Mdp                  TEXT    NOT NULL," +
 	                   " TypeUser             TEXT    NOT NULL," +
 	                   " Pseudo2              TEXT    NOT NULL," +
-	                   " CONSTRAINT Pseudo2_fk FOREIGN KEY(Pseudo2) REFERENCES Probleme)")
+	                   " CONSTRAINT Pseudo2_fk FOREIGN KEY(Pseudo2) REFERENCES levels)")
 	void createUtilisateurTable();
 	
 	@SqlUpdate("insert into Utilisateur (Pseudo, Nom, Prenom, Mdp, TypeUser) values (:Pseudo, :Nom, :Prenom, :Mdp, :TypeUser)")
@@ -51,7 +50,7 @@ public interface UserDao {
 	@RegisterMapperFactory(BeanMapperFactory.class)
 	UserData verifUser(@Bind("Pseudo") String Pseudo, @Bind("Mdp") String Mdp);
 	
-	@SqlQuery("select sum(Score) from Probleme as p, Utilisateur as a where p.Pseudo = a.Pseudo;")
+	@SqlQuery("select sum(Score) from Historique as h, Utilisateur as a where h.NumUt = a.NumUt;")
 	@RegisterMapperFactory(BeanMapperFactory.class)
 	int ScoreTotal(@Bind("Score") int Score);
 	
