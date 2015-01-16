478c7737717a7de8428aebec23c0aa2580c9e415
Baptiste Marbaix <baptiste.marbaix@etudiant.univ-lille1.fr>
2015-01-16 09:22:55 +0100

    classe test historic

---
+++ src/main/java/fr/iutinfo/Score.java
@@ -0,0 +1,14 @@
+package fr.iutinfo;
+
+public class Score {
+	private int value;
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int somme) {
+		this.value = somme;
+	}
+	
+}
